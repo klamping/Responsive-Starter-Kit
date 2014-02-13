14f6295bcd68942ab3eb9c1dee316e2008488395
Michael Gilbert <mpgilbertusa@gmail.com>
2014-02-13 11:06:31 -0600

    removed plugin JS functions; added helper note

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -5,34 +5,9 @@
 
 --------------------------------- */
 
-// General purpose functions
 $(document).ready(function () {
 
-    // modal for general content (fancyBox)
-    if ($(".fancybox").length > 0) {
-        $(".fancybox").fancybox({
-            maxWidth: 800,
-            maxHeight: 600
-        });
-    }
-
-    // modal for video player (fancyBox)
-    if ($('.fancybox-media').length > 0) {
-        $('.fancybox-media').fancybox({
-            width: 800,
-            height: 450,
-            padding: 2,
-            openEffect: 'none',
-            closeEffect: 'none',
-            helpers: {
-                media: {}
-            }
-        });
-    }
-
-    // flexible videos (fitVids)
-    if ($('.flexible-video').length > 0) {
-        $(".flexible-video").fitVids();
-    }
+    // Place general purpose functions here.
+    // This might also be a good place for plugin functions.
     
 });
